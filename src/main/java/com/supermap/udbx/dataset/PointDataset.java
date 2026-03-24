425c08f60d86c91f198720dcbcd6856e6e37ed28
rainstop_3 <zhyt1985@163.com>
2026-03-25 01:06:15 +0800

    fix: add missing Statement import to PointDataset

--- src/main/java/com/supermap/udbx/dataset/PointDataset.java
+++ src/main/java/com/supermap/udbx/dataset/PointDataset.java
@@ -13,6 +13,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
